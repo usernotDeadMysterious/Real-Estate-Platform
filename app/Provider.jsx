e4f70bf3f34f4aba37259dd06496a8ab3dc5234c
usernotDeadMysterious <you@example.com>
2025-02-20 17:27:19 +0500

    logo updated

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -17,7 +17,7 @@ function Provider({children}) {
         <JustAnotherNav/>
 
         {/* <Navbar/> */}
-        <div className='mt-12'>
+        <div className='mt-20'>
         {children}
         </div>
         <Footer/>
