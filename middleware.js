7e9b6c7684375a1a95c4cea55321d5665b2b03c8
usernotDeadMysterious <you@example.com>
2025-04-03 15:10:53 +0500

    Auth removed for /rent /for-sale and /about-us only

--- middleware.js
+++ middleware.js
@@ -3,7 +3,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)',
-    '/sign-up(.*)'])
+    '/sign-up(.*)',
+    '/rent$',         // Only /rent (not /rent/*)
+    '/for-sale$',     // Only /for-sale (not for-sale/*)
+    '/about-us$'      // Only /about-us (not /about-us/*)
+  ])
 
 export default clerkMiddleware(async (auth, request) => {
   
